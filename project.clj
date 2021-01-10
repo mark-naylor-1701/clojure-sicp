663894e47a3a90f005ce7aa0af0cdf644b19da79
Mark W. Naylor <mark.naylor.1701@gmail.com>
2021-01-10 18:57:00 -0500

    Initial check-in

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.mark-naylor-1701.sicp "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "3 Clause BSD"
+            :url "https://"}
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns org.mark-naylor-1701.sicp})
---
+++ src/org/mark_naylor_1701/sicp.clj
@@ -0,0 +1,6 @@
+(ns org.mark-naylor-1701.sicp)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
