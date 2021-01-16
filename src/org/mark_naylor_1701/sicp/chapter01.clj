c18034d4782ee76198db395c4694ee1388ba15b3
Mark W. Naylor <mark.naylor.1701@gmail.com>
2021-01-16 17:46:39 -0500

    Add exercise 1.8
    * Description of the exercise.
    * Support functions for testing purposes.

--- src/org/mark_naylor_1701/sicp/chapter01.clj
+++ src/org/mark_naylor_1701/sicp/chapter01.clj
@@ -119,6 +119,29 @@
 (defn sqrt-2
   [x]
   (sqrt-iter-2 1.0 x -1.0))
+
+;; Exercise 1.8. Newton's method for cube roots is based on the fact
+;; that if y is an approximation to the cube root of x, then a better
+;; approximation is given by the value:
+
+;; (x/y^2 + 2y) / 3
+
+;; Use this formula to implement a cube-root procedure analogous to
+;; the square-root procedure. (In section 1.3.4 we will see how to
+;; implement Newton's method in general as an abstraction of these
+;; square- root and cube-root procedures.)
+
+(defn- cube
+  "x raised to the 3rd power."
+  [x]
+  (* x x x))
+
+(defn- cube-root-native
+  "Cube root function using native Math/pow function."
+  [x]
+  (Math/pow x 1/3))
+
+
 ;; ------------------------------------------------------------------------------
 ;; BSD 3-Clause License
 
