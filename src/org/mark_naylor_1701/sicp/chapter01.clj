b0f8198adc0f6f9b7e6b970eb79b6de2cd694c65
Mark W. Naylor <mark.naylor.1701@gmail.com>
2021-01-17 03:28:14 -0500

    Add exercise 1.10
    Add docstring to defn k

--- src/org/mark_naylor_1701/sicp/chapter01.clj
+++ src/org/mark_naylor_1701/sicp/chapter01.clj
@@ -165,6 +165,46 @@
   ""
   [x]
   (cube-root-iter 1 x))
+
+
+;;Exercise 1.10. The following procedure computes a mathematical function called Ackermann's function.
+;; (define (A x y)
+;;   (cond ((= y 0) )
+;;         ((= x 0) (* 2 y))
+;;         ((= y 1) 2)
+;;         (else (A
+;;                (- x 1)
+;;                (A x (- y 1))))))
+
+(defn A
+  "Ackermann's function."
+  [x y]
+  (cond
+    (zero? y) 0
+    (zero? x) (* 2 y)
+    (= y 1) 2
+    :else (A (dec x) (A x (dec y)))))
+
+;; What are the values of the following expressions?
+;; (A 1 10)    -> 1024
+;; (A 2 4)     -> 65536
+;; (A 3 3)     -> 65536
+
+;; Consider the following procedures, where A is the procedure defined above:
+;; (define (f n) (A 0 n))
+;; (define (g n) (A 1 n))
+;; (define (h n) (A 2 n))
+;; (define (k n) (* 5 n n))
+;; Give concise mathematical definitions for the functions computed by the procedures f, g, and h for
+;; positive integer values of n. For example, (k n) computes 5n2.
+
+(def f "Partially applied A on 0" (partial A 0))
+(def g "Partially applied A on 1" (partial A 1))
+(def h "Partially applied A on 2" (partial A 2))
+(defn k [n] "Computes 5 * n^2" (* 5 n n))
+
+
+
 ;; ------------------------------------------------------------------------------
 ;; BSD 3-Clause License
 
